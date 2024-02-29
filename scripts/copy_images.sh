9ed2984866b5403d3b1134981a8c5fddf70b589a
Damian Jaspar <damianjaspar@mailbox.org>
2024-02-29 21:43:32 +0100

    Updates before going public

--- scripts/copy_images.sh
+++ scripts/copy_images.sh
@@ -3,25 +3,20 @@
 # Assign input file (containing subjects with id and cohort) to variable
 file=$1
 
-source_path="/scratch/hbndetdata/MRI/derivatives/sMRIprep"
-target_path="/scratch/hbndetdata/brain-images"
-
-declare -a files=(\
-"_acq-HCP_desc-brain_mask.nii.gz"\
-"_acq-HCP_desc-preproc_T1w.nii.gz"\
-"_acq-HCP_label-GM_probseg.nii.gz"\
-"_acq-HCP_label-WM_probseg.nii.gz"\
-"_acq-HCP_label-CSF_probseg.nii.gz"\
-"_acq-HCP_space-MNIPediatricAsym_cohort-"*"_desc-preproc_T1w.nii.gz"\
-"_acq-HCP_space-MNIPediatricAsym_cohort-"*"_label-GM_probseg.nii.gz"\
-"_acq-HCP_space-MNIPediatricAsym_cohort-"*"_label-WM_probseg.nii.gz"\
-"_acq-HCP_space-MNIPediatricAsym_cohort-"*"_label-CSF_probseg.nii.gz"\
-)
+source_path="user@client:/scratch/hbnetdata/derivatives/smriprep"
+target_path="/project/t1images"
 
 # Iterate over subject ids
-for id in $(cat "$file"); do
+for line in $(cat "$file"); do
+	id=$(echo "${line}" | awk -F '-' '{print $1}')
+	cohort=$(echo "${line}" | awk -F '-' '{print $2}')
 	# Copy files
-	for f in "${files[@]}"; do
-		$(cp "$source_path"/sub-"$id"/anat/sub-"$id""$f" "$target_path")
-	done
+	# Normal case
+	base="${source_path}/sub-${id}/anat/sub-${id}_acq-HCP_space-MNIPediatricAsym_cohort-"
+	# Staten Island case
+	#base="${source_path}/sub-${id}/anat/sub-${id}_space-MNIPediatricAsym_cohort-"
+	scp "${base}${cohort}_desc-preproc_T1w.nii.gz" "${target_path}"
+	scp "${base}${cohort}_label-GM_probseg.nii.gz" "${target_path}"
+	scp "${base}${cohort}_label-WM_probseg.nii.gz" "${target_path}"
+	scp "${base}${cohort}_label-CSF_probseg.nii.gz" "${target_path}"
 done
